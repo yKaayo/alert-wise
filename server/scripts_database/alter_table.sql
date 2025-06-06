dd3e01f11b790848f8731c5b50aee240db9dd9dd
yKaayo <caiokaea@gmail.com>
2025-06-06 19:23:12 -0300

    fix: alter table fixed

--- server/scripts_database/alter_table.sql
+++ server/scripts_database/alter_table.sql
@@ -1,24 +1,29 @@
 alter table publicacao_gs
    add constraint fk_usuario_publicacao
       foreign key ( id_usuario )
-         references usuario_gs ( id );
+         references usuario_gs ( id )
+            on delete cascade;
 
 alter table comentario_gs
    add constraint fk_usuario_comentario
       foreign key ( id_usuario )
-         references usuario_gs ( id );
+         references usuario_gs ( id )
+            on delete cascade;
 
 alter table comentario_gs
    add constraint fk_publicacao_comentario
       foreign key ( id_publicacao )
-         references publicacao_gs ( id );
+         references publicacao_gs ( id )
+            on delete cascade;
 
 alter table area_risco_gs
    add constraint fk_usuario_area_risco
       foreign key ( id_usuario )
-         references usuario_gs ( id );
+         references usuario_gs ( id )
+            on delete cascade;
 
 alter table jogo_gs
    add constraint fk_usuario_jogo
       foreign key ( id_usuario )
-         references usuario_gs ( id );
+         references usuario_gs ( id )
+            on delete cascade;
---
+++ server/scripts_database/drop.sql
@@ -0,0 +1,5 @@
+drop table jogo_gs;
+drop table area_risco_gs;
+drop table comentario_gs;
+drop table publicacao_gs;
+drop table usuario_gs;
